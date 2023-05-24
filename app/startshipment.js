cc12ec6bd21d4a43dcefd3b5e9f906a28ac3fdbe
Asileka <asilkatya@gmail.com>
2023-05-24 02:37:23 +0100

    fix: now takes shipment type into the account

--- app/startshipment.js
+++ app/startshipment.js
@@ -1,10 +1,11 @@
 import { View, Text, Button } from "react-native";
 import { Link, useNavigation, useRouter } from "expo-router";
 import Info1 from "../components/Info1";
+import { useState } from "react";
 export default function StartShipment() {
   const router = useRouter();
   const navigation = useNavigation();
-
+  const [value, setValue] = useState("International");
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Define your package</Text>
