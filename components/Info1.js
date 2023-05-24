18963011e06edb7ad26497e702f1746dc79ddd3f
Asileka <asilkatya@gmail.com>
2023-05-24 02:36:50 +0100

    fix: now takes shipment type into the account

--- components/Info1.js
+++ components/Info1.js
@@ -3,6 +3,21 @@ export default function Info1() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Where are you wanting to ship?</Text>
+      <Radio.Group
+        name="shipmentMethod"
+        accessibilityLabel="shipment method"
+        shipmentMthodValue={shipmentMthodValue}
+        onChange={(nextShipmentMethod) => {
+          setShipmentMethod(nextShipmentMethod);
+        }}
+      >
+        <Radio shipmentMthodValue="one" my={1}>
+          One
+        </Radio>
+        <Radio shipmentMthodValue="two" my={1}>
+          Two
+        </Radio>
+      </Radio.Group>
     </View>
   );
 }
